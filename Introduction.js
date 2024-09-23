c2c2ba016b642aefe38bba4ca5a92eed6473ab6f
eunkyoung529 <eunkyoung4160@gmail.com>
2024-09-23 22:26:47 +0900

    Introduction.js

---
+++ Introduction.js
@@ -0,0 +1,57 @@
+//Opening line.
+/*console.log('It was love at first sight.');
+
+console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');*/
+
+
+console.log("JavaScript");
+console.log(2011);
+console.log("Woohoo! I love to code! #codecademy");
+console.log(20.49);
+
+
+//산술연산자
+console.log(22+3.5);
+console.log(2024-1969);
+console.log(65/240);
+console.log(100*0.2708);
+
+//문자열 연결
+console.log('Hello' + 'World');
+console.log('Hello' + " " +"World");
+
+//속성
+console.log("Teaching the world how to code" .length);
+
+//Methods
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy');
+console.log('Codecademy'.toUpperCase());
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   ');
+console.log('    Remove whitespace   '.trim());
+
+//내장객체
+console.log(Math.random()*100);
+console.log(Math.floor(Math.random()*100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+
+//변수 생성
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
+
+const entree = 'Enchiladas';
+console.log(entree);
+entree = 'Tacos';
